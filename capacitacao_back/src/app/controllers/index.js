faca4c502a3370bc97cb52d9808858f8aff9cfb6
Camily de Bem <80923475+camidebem@users.noreply.github.com>
2023-08-05 15:07:21 -0300

    Add files via upload

---
+++ capacitacao_back/src/app/controllers/index.js
@@ -0,0 +1,4 @@
+import Agenda from './agenda';
+import Auth from './Auth';
+import Uploads from './Uploads';
+export { Agenda, Auth, Uploads };
---
+++ capacitacao_back/src/config/Auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: 'HUDSKOFSO4785H$%#-$*9EFD+H867489EHDF-U32478%¨#*()@*DJD'
+
+};
